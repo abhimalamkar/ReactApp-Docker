1150601cb71bd6b1fb5db0e529db1ca75f610013
Abhijeet Malamkar <abhijeetmalamkar@gmail.com>
2020-01-15 17:35:59 -0500

    first commit

---
+++ app/src/AppAuth.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import firebase from 'firebase'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCgHldN657sZsi8-lkUVEQ4j2n0KyTySsU",
+  authDomain: "quotus-204705.firebaseapp.com"
+})
+
+class App extends React.Component {
+  state = { isSignedIn: false }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      // console.log("user", user)
+    })
+  }
+
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            {/* <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            /> */}
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default App;
